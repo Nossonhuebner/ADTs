be2d36c49269b6f80cc9917a48a19c5c4f8a3a2a
nosson <nossonhuebner@gmail.com>
2018-08-30 14:57:28 -0400

    Create IntSet class

--- Ruby/int_set.rb
+++ Ruby/int_set.rb
@@ -1,4 +1,5 @@
 class MaxIntSet
+  #contains a predetermined range of positive integers. Initialized with the max value.
   def initialize(max)
     @store = Array.new(max, false)
   end
@@ -28,5 +29,35 @@ class MaxIntSet
 
   def validate!(num)
     raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  #contains an arbitrary range of numbers, with a fixed number of buckets to store them in
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num unless self.include?(num)
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num) #checks corresponding bucket
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets] #returns corresponding bucket
+  end
+
+  def num_buckets
+    @store.length
   end
 end
